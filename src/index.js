13384358e91f143b15824f57c66978169f4e5207
Dmitriy Pismenniy <dima30655@gmail.com>
2020-04-04 20:09:45 +0300

    module 12

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import debounce from 'lodash.debounce';
 import axios from 'axios';
+import PNotify from '../node_modules/pnotify/dist/es/PNotify';
 import template from './templates/countryTable.hbs';
 
 const input = document.querySelector( `.search` );
@@ -23,6 +24,17 @@ function getCounties ( event ) {
             } else if( data.length > 1 && data.length <= 10 ) {
                 const markup = data.reduce( ( acc, el ) => `${acc}` + `<li class="countries-list">${el.name}</li>`, `` );
                 ul.insertAdjacentHTML( `beforeend`, markup );
+            } else if( data.length > 10 ) {
+                PNotify.error( {
+                    text: "Too many matches found. Please enter a more specific query!",
+                    delay: 8000,
+                    addClass: `warning`,
+                    width: '250px',
+                    remove: true,
+                    stack: {
+                        context: ul
+                    },
+                } );
             }
         } )
         .catch( err => {
